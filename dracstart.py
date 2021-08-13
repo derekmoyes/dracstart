2f8e9e948cfa4f5233788a3347464216265d7769
Derek Moyes <derek.moyes@rackspace.com>
2021-08-13 12:06:15 -0500

    Some cleanup.

--- dracstart.py
+++ dracstart.py
@@ -44,9 +44,11 @@ SOFTWARE. https://opensource.org/licenses/MIT
 import argparse
 import textwrap 
 import glob
-import os 
+import os
+import sys
 from datetime import datetime 
 from time import sleep 
+
 
 # Adjust these variables for your environment. 
 DebugString        = "==> DEBUG:"
@@ -56,6 +58,7 @@ Snippet            = "drac"
 JavaRunner         = "/usr/bin/javaws"
 DeleteFileAfterUse = 1
 DeleteAfterSeconds = 15 
+
 
 # Setup command-line arguments. 
 verbose = False
@@ -75,70 +78,79 @@ parser.add_argument(
     help="Print additional debug information")
 args = parser.parse_args()
 
-if args.verbose == True:
-  verbose = True
-  print("Setting verbose to %s." % verbose)
+if args.verbose:
+    verbose = True
+    print("Setting verbose to %s." % verbose)
 
-if args.debug == True:
-  debug = True
-  print("Setting debug to %s." % debug)
+if args.debug:
+    debug = True
+    print("Setting debug to %s." % debug)
+
 
 # Begin. 
 # Locate the latest FileExt file in DownloadsPath.
 if verbose:
-  print("Checking for new %s files in %s." % (FileExt, DownloadsPath))
+    print("Checking for new %s files in %s." % (FileExt, DownloadsPath))
 
 list_of_files = glob.glob(DownloadsPath + '*' + FileExt) 
 
 if not list_of_files:
-  if verbose:
-    print("Specific %s file not found, checking all files in %s." % (FileExt, DownloadsPath))
-  list_of_files = glob.glob(DownloadsPath + '*')
-  try:
-    latest_file = max(list_of_files, key=os.path.getctime)
-  except ValueError as ve:
-    raise SystemExit("Sorry, I don't see any files in %s." % DownloadsPath)
+    if verbose:
+        print("Specific %s file not found, checking all files in %s." % (
+             FileExt, DownloadsPath))
+    list_of_files = glob.glob(DownloadsPath + '*')
+    try:
+        latest_file = max(list_of_files, key=os.path.getctime)
+    except ValueError as ve:
+        raise SystemExit("Sorry, I don't see any files in %s." % DownloadsPath)
 else:
-  try:
-    latest_file = max(list_of_files, key=os.path.getctime)
-  except ValueError as ve:
-    raise SystemExit("Sorry, I don't see any files in %s." % DownloadsPath)
+    try:
+        latest_file = max(list_of_files, key=os.path.getctime)
+    except ValueError as ve:
+        raise SystemExit("Sorry, I don't see any files in %s." % DownloadsPath)
 
 if verbose:
-  print("Found newest file %s." % (latest_file))
+    print("Found newest file %s." % (latest_file))
+
 
 # Validate that it is a DRAC config file.
 try:
-  validate = open(latest_file, 'r').readlines()
+    validate = open(latest_file, 'r').readlines()
 except IOError as err:
     print("Error: %s" % err)
 
 matches = [] 
 for word in validate:
-  if Snippet in word.lower():
-    matches.append(word)
+    if Snippet in word.lower():
+        matches.append(word)
 if debug:
-  print("%s List of read lines matching %s: " % (DebugString, Snippet))
-  print("%s %s" % (DebugString, matches))
+    print("%s List of read lines matching %s: " % (DebugString, Snippet))
+    print("%s %s" % (DebugString, matches))
 
 if not matches:
-  raise SystemExit("Sorry, the newest file %s doesn't seem to be of %s type." % (latest_file, FileExt))
+    raise SystemExit("Sorry, the newest file %s doesn't seem to be of %s type." 
+                    % (latest_file, FileExt))
   
+
 # Rename it to something we can use. 
 dt = datetime.now().strftime('%Y%m%d%M%S')
 new_file_name = DownloadsPath + Snippet + dt + "." + FileExt
 os.rename(latest_file, new_file_name)
 if verbose:
-  print("Renamed to: %s" % (new_file_name))
+    print("Renamed to: %s" % (new_file_name))
+
 
 # Run it with JavaRunner. 
 builtcommand = JavaRunner + " " + new_file_name
 print("Starting: %s" % builtcommand)
 os.system(builtcommand)
 
+
 # Optionally move the file to trash after a specified period. 
 if DeleteFileAfterUse:
-  print("Waiting %s seconds to remove %s." % (DeleteAfterSeconds, new_file_name))
-  sleep(DeleteAfterSeconds)
-  os.remove(new_file_name)
-  print("Removed.")
+    print("Waiting %s seconds to remove %s..." % (
+                DeleteAfterSeconds, new_file_name))
+    sleep(DeleteAfterSeconds)
+    os.remove(new_file_name)
+    print("Removed.")
+
